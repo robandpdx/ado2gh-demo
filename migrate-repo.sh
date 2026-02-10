3251da6e7510c965f07615f236b78fc7de75a217
Rob Anderson <robandpdx@github.com>
2026-02-10 10:59:17 -0600

    add script to migrate single repo

--- migrate-repo.sh
+++ migrate-repo.sh
@@ -6,9 +6,9 @@ else
     LOCK_SOURCE_REPO_FLAG=""
 fi
 
-# Generate a script for all repositories in a project with all options
-gh ado2gh generate-script \
+gh ado2gh migrate-repo \
     --ado-org $SOURCE_ORG \
     --ado-team-project $SOURCE_TEAM_PROJECT \
+    --ado-repo $SOURCE_REPO \
     --github-org $TARGET_ORG \
-    --all $LOCK_SOURCE_REPO_FLAG
+    --github-repo $TARGET_REPO $LOCK_SOURCE_REPO_FLAG
