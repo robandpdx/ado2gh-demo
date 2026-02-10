06fdc914983d942d33669c5f9b98cde74162a5de
Rob Anderson <robandpdx@github.com>
2026-02-10 11:28:45 -0600

    fix ADO repo lock

--- migrate-repo.sh
+++ migrate-repo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ "$LOCK_SOURCE_REPO" = "true" ]; then
-    LOCK_SOURCE_REPO_FLAG="--lock-ado-repo"
-else
-    LOCK_SOURCE_REPO_FLAG=""
+    LOCK_SOURCE_REPO_FLAG="lock-ado-repo"
+    gh ado2gh lock-ado-repo \
+        --ado-org $SOURCE_ORG \
+        --ado-team-project $SOURCE_TEAM_PROJECT \
+        --ado-repo $SOURCE_REPO
 fi
 
 gh ado2gh migrate-repo \
@@ -11,4 +13,4 @@ gh ado2gh migrate-repo \
     --ado-team-project $SOURCE_TEAM_PROJECT \
     --ado-repo $SOURCE_REPO \
     --github-org $TARGET_ORG \
-    --github-repo $TARGET_REPO $LOCK_SOURCE_REPO_FLAG
+    --github-repo $TARGET_REPO
