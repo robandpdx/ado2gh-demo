4e047baa753093b30578cf7b8b9d23c301bcf2ca
Rob Anderson <robandpdx@github.com>
2026-02-10 10:47:58 -0600

    add lock repo flag

--- generate-script.sh
+++ generate-script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+if [ "$LOCK_SOURCE_REPO" = "true" ]; then
+    LOCK_SOURCE_REPO_FLAG="--lock-ado-repo"
+else
+    LOCK_SOURCE_REPO_FLAG=""
+fi
+
 # Generate a script for all repositories in a project with all options
 gh ado2gh generate-script \
     --ado-org $SOURCE_ORG \
     --ado-team-project $SOURCE_REPO \
     --github-org $TARGET_ORG \
-    --all
+    --all $LOCK_SOURCE_REPO_FLAG
